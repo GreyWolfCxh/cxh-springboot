3372b278c54da35f50fe4699434583ec5afa6dcf
GreyWolfCxh <1278826642@qq.com>
2021-12-28 16:45:05 +0800

    springboot实现多文件上传

---
+++ file-upload-more/src/main/java/com/cxh/fileuploadmore/FileUploadMoreApplication.java
@@ -0,0 +1,13 @@
+package com.cxh.fileuploadmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadMoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadMoreApplication.class, args);
+    }
+
+}
---
+++ file-upload-more/src/test/java/com/cxh/fileuploadmore/FileUploadMoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxh.fileuploadmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadMoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
