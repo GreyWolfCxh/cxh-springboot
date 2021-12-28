b9d6190883d0e3f16366bb4a0b17f79db36611b1
GreyWolfCxh <1278826642@qq.com>
2021-12-28 17:01:24 +0800

    springboot使用@Scheduled实现定时任务

---
+++ schedule/src/main/java/com/cxh/schedule/task/ScheduledTasks.java
@@ -0,0 +1,22 @@
+package com.cxh.schedule.task;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class ScheduledTasks {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("现在时间：" + dateFormat.format(new Date()));
+    }
+
+}
