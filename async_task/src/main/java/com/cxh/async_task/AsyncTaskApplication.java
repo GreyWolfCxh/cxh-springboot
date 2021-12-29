b0c5d4e993533cf580cbf0e2a91e6233256684ea
GreyWolfCxh <1278826642@qq.com>
2021-12-29 16:29:38 +0800

    springboot使用@Async实现异步调用

---
+++ async_task/src/main/java/com/cxh/async_task/AsyncTaskApplication.java
@@ -0,0 +1,22 @@
+package com.cxh.async_task;
+
+import com.cxh.async_task.task.AsyncTasks;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.CompletableFuture;
+
+@EnableAsync
+@SpringBootApplication
+public class AsyncTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncTaskApplication.class, args);
+
+    }
+
+}
---
+++ async_task/src/test/java/com/cxh/async_task/AsyncTaskApplicationTests.java
@@ -0,0 +1,36 @@
+package com.cxh.async_task;
+
+import com.cxh.async_task.task.AsyncTasks;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@SpringBootTest
+class AsyncTaskApplicationTests {
+
+    @Autowired
+    private AsyncTasks asyncTasks;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void Test() throws Exception {
+        long start = System.currentTimeMillis();
+
+        CompletableFuture<String> task1 = asyncTasks.doTaskOne();
+        CompletableFuture<String> task2 = asyncTasks.doTaskTwo();
+        CompletableFuture<String> task3 = asyncTasks.doTaskThree();
+
+        CompletableFuture.allOf(task1, task2, task3).join();
+
+        long end = System.currentTimeMillis();
+
+        log.info("任务全部完成，总耗时：" + (end - start) + "毫秒");
+    }
+}
